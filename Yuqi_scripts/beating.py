a8ca0922760ae36d413444b731917a3d40213631
yuqimiao <ym2771@cumc.columbia.edu>
2020-04-17 08:43:22 -0500

    tail_bearing and clustering

--- Yuqi_scripts/beating.py
+++ Yuqi_scripts/beating.py
@@ -16,16 +16,15 @@ def rotation(data):
     n = data.shape[0]
     ## transfer spline data to point vector
     spline_point = []
-    x_index = [i for i in range(15,15+7*3) if (i+1)%3 == 1]
-    y_index = [i for i in range(15,15+7*3) if (i+1)%3 == 2]
-    for j in range(n):
-        x = data.iloc[j,x_index]
-        y = data.iloc[j,y_index]
-        spline_point.append(np.column_stack([x,y]))
+    x_index = [0, 15, 18, 21, 24, 27, 30, 33]
+    y_index = [1, 16, 19, 22, 25, 28, 31, 34]
+    x = data.iloc[:,x_index]
+    y = data.iloc[:,y_index]
+    spline_point = np.column_stack([x,y])
     
     ## reference vector
-    head = np.column_stack([data["A_head"]["x"],data["A_head"]["y"]]) # dim = 216059, 2
-    spline1 = np.column_stack([data["F_spine1"]["x"],data["F_spine1"]["y"]])
+    head = np.column_stack([data.iloc[:,0],data.iloc[:,1]]) # dim = 216059, 2
+    spline1 = np.column_stack([data.iloc[:,15],data.iloc[:,16]])
     # dim = 216059, 2
     head_r = head-spline1 # reference vector to x axis
     
@@ -42,21 +41,39 @@ def rotation(data):
     spline_rotate = []
     for i in range(n):
         x = []
-        for j in spline_point[i]:
-            x.append(np.dot(rot_matrix[1].reshape(2,2),j-spline1[i]))
+        for j in spline_point[i].reshape((8,2), order = "F"):
+            x.append((np.dot(rot_matrix[i].reshape(2,2),j-spline1[i])))
         spline_rotate.append(x)
     
     return(spline_rotate)
     
     
-def tail_spline(rotate_points):
+def tail_spline(rotate_points,r = 15, t1 = 10, t2 = 5):
     tail = []
+    j=0
     for i in range(len(rotate_points)):
         pts = np.array(rotate_points[i])
-        tck, u = interpolate.splprep(pts.T, u=None, s=0.0, per=1)
-        x = np.vstack(rotate_points[i])[:,0]
-        yder = interpolate.splev(x, tck, der=1)
-        tail.append(yder)
+        index = [0]
+        omit = []
+
+        for k in range(len(pts)):
+            radius = np.linalg.norm(pts[k])
+            if radius<=(r*(k-1)+t1) and radius >(r*(k-2)-t2):
+                index.append(k)
+            else:
+                omit.append(k)
+        if len(index)<4:
+            print(i)
+            continue        
+        pts_n = pts[index]
+        f = pts[max(index)][1]/pts[max(index)][0]
+        tck, u = interpolate.splprep(pts_n.T, u=None, s=0.0) 
+        yder = interpolate.splev(u, tck, der=1)
+        z = np.zeros(8)
+        z[index] = (yder[1]/yder[0])
+        z[omit[1:]] = f
+        tail.append(z)
+    tail = np.array(tail)
     return(tail)
         
     
